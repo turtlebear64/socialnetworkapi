9d97f7f90575d73c1fa8aba4237154886cfd3e7a
Nicolas Danciu <nixrom1@gmail.com>
2022-04-08 17:27:04 -0400

    fixed some file naming and some controller data

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,36 +1,36 @@
-const { Logic } = require('../models/Logic');
+const { Thought } = require('../models/Thought');
 const User = require('../models/User');
 
-const logicController = {
+const thoughtController = {
     getAllThoughts(req, res) {
-        Logic.find({})
-            .then(dbLogicData => res.json(dbLogicData))
+        Thought.find({})
+            .then(dbThoughtData => res.json(dbThoughtData))
             .catch(err => {
             console.log(err);
              res.status(400).json(err);
             });
     },
-    getLogicById({ params }, res) {
-        Logic.findOne({ _id: params.logicId })
-            .then(dbLogicData => {
-                if (!dbLogicData) {
+    getThoughtById({ params }, res) {
+        Thought.findOne({ _id: params.thoughtId })
+            .then(dbThoughtData => {
+                if (!dbThoughtData) {
                  res.status(404).json({ message: 'Nothing was found' });
                 return;
                 }
-                res.json(dbLogicData);
+                res.json(dbThoughtData);
             })
             .catch(err => {
                 console.log(err);
                 res.status(400).json(err);
             });
     },
-    addLogic({ params, body }, res) {
+    addThought({ params, body }, res) {
         console.log(body);
-        Logic.create(body)
+        Thought.create(body)
             .then(({ _id }) => {
                 return User.findOneAndUpdate(
                  { _id: params.userId },
-                 { $push: { logic: _id } },
+                 { $push: { thought: _id } },
                 { new: true }
                 );
             })
@@ -44,25 +44,25 @@ const logicController = {
             .catch(err => res.json(err));
     },
     updateLogic({ params, body }, res) {
-        Logic.findOneAndUpdate({ _id: params.logicId }, body, { new: true, runValidators: true })
-            .then(dbLogicData => {
-                if (!dbLogicData) {
+        Thought.findOneAndUpdate({ _id: params.thoughtId }, body, { new: true, runValidators: true })
+            .then(dbThoughtData => {
+                if (!dbThoughtData) {
                  res.status(404).json({ message: 'Nothing was found' });
                 return;
                 }
-                res.json(dbLogicData);
+                res.json(dbThoughtData);
             })
             .catch(err => res.status(400).json(err));
     },
-    removeLogic({ params }, res) {
-        Logic.findOneAndDelete({ _id: params.thoughtId })
-            .then(deletedLogic => {
-                if (!deletedLogic) {
+    removeThought({ params }, res) {
+        Thought.findOneAndDelete({ _id: params.thoughtId })
+            .then(deletedThought => {
+                if (!deletedThought) {
                     return res.status(404).json({ message: 'Nothing found' });
                 }
                 return User.findOneAndUpdate(
                 { _id: params.userId },
-                { $pull: { logic: params.thoughtId } },
+                { $pull: { thought: params.thoughtId } },
                 { new: true }
                 );
             })
@@ -81,17 +81,17 @@ const logicController = {
         { $addToSet: { reactions: body } },
         { new: true, runValidators: true }
         )
-            .then(dbLogicData => {
-                if (!dbLogicData) {
+            .then(dbThoughtData => {
+                if (!dbThoughtData) {
                  return res.status(404).json({ message: 'Nothing found' });
                 }
-                res.json(dbLogicData);
+                res.json(dbThoughtData);
             })
             .catch(err => res.json(err));
     },
     removeReaction({ params }, res) {
         console.log(params.thoughtId, params.reactionId);
-        Logic.findOneAndUpdate(
+        Thought.findOneAndUpdate(
             { _id: params.thoughtId },
             { $pull: { reactions: { reactionId: params.reactionId } } },
             { runValidators: true, new: true }
